a0e40a96319446e362facb625647a5796f60674a
Mark <usertzc@gmail.com>
2017-03-31 13:36:46 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,4 +1,4 @@
-echo 'UserParameter=iptables_lins,/usr/bin/sudo iptables -S|wc -l' >> /etc/zabbix/zabbix_agentd.conf && \
+echo "UserParameter=iptables_lins,/usr/bin/sudo iptables -S |md5sum|awk '{print $1}'" >> /etc/zabbix/zabbix_agentd.conf && \
 echo 'zabbix ALL=(root)NOPASSWD:/usr/sbin/iptables,/usr/bin/cksum /etc/sysconfig/iptables' >>/etc/sudoers &&\
 echo 'UserParameter=iptables_file,/usr/bin/sudo /usr/bin/cksum /etc/sysconfig/iptables'  >>/etc/zabbix/zabbix_agentd.conf && \
 sed -i 's/Defaults    requiretty/#Defaults    requiretty/g' /etc/sudoers && cat /etc/sudoers|grep Defaults && \
