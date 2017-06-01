523e489a22fe8a92720d98a8ddae9b4f2b1b5f29
Mark <usertzc@gmail.com>
2017-06-01 15:45:49 +0800

    Create swap.sh

---
+++ add_swap/swap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dd if=/dev/zero of=/swap_file bs=2G count=1
+chmod 600 /swap_file
+/sbin/mkswap -f /swap_file 
+/sbin/swapon /swap_file
+echo "/swap_file swap swap defaults 0 0" >> /etc/fstab
