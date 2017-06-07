bbad24676c156130f9f7357fdf10f758a16186b3
Mark <usertzc@gmail.com>
2017-06-07 18:44:09 +0800

    Update install_db_status.sh

--- mysql_QPTS/mariadb/install_db_status.sh
+++ mysql_QPTS/mariadb/install_db_status.sh
@@ -1,5 +1,6 @@
-DPAT2=/etc/zabbix/scripts/
+#!/bin/bash
+DPAT2=/etc/zabbix/scripts
 mkdir -p $DPAT2
-curl -LK https://raw.githubusercontent.com/LinuxEA-Mark/zabbix3.0.2-complete-works/master/mysql_QPTS/mariadb/mariadb.sh -o $DPAT2 
+curl -LK https://raw.githubusercontent.com/LinuxEA-Mark/zabbix3.0.2-complete-works/master/mysql_QPTS/mariadb/mariadb.sh -o $DPAT2/mariadb.sh
 curl -Lk https://raw.githubusercontent.com/LinuxEA-Mark/zabbix3.0.2-complete-works/master/mysql_QPTS/mariadb/zabbixmy.conf -o /etc/zabbix/zabbixmy.conf
 echo "UserParameter=maria.db[*],/etc/zabbix/scripts/mariadb.sh" >> /etc/zabbix/zabbix_agentd.conf
