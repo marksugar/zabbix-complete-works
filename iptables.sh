fcb1e22eac0b98edc0236c9cbdaa2e3c3741152e
Mark <usertzc@gmail.com>
2017-06-08 16:13:50 +0800

    Create iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,6 +1,6 @@
-echo "UserParameter=iptables_lins,/usr/bin/sudo iptables -S |md5sum|awk '{print $1}'" >> /etc/zabbix/zabbix_agentd.conf && \
+echo "UserParameter=iptables_lins,/usr/bin/sudo iptables -S |md5sum|awk '{print \$1}'" >> /etc/zabbix/zabbix_agentd.conf && \
 echo 'zabbix ALL=(root)NOPASSWD:/usr/sbin/iptables,/usr/bin/cksum /etc/sysconfig/iptables' >>/etc/sudoers &&\
-echo 'UserParameter=iptables_file,/usr/bin/sudo /usr/bin/cksum /etc/sysconfig/iptables'  >>/etc/zabbix/zabbix_agentd.conf && \
+echo 'UserParameter=iptables_file,/usr/bin/sudo /usr/bin/cksum /etc/sysconfig/iptables'|awk '{print \$1}'  >>/etc/zabbix/zabbix_agentd.conf && \
 sed -i 's/Defaults    requiretty/#Defaults    requiretty/g' /etc/sudoers && cat /etc/sudoers|grep Defaults && \
 systemctl restart zabbix-agent
 #sed -i '/delete/d' 
