d0778587d482a9feb097719b9020d4ece5902293
Mark <usertzc@gmail.com>
2017-07-12 17:17:37 +0800

    Update mq.sh

--- zk-mq/mq.sh
+++ zk-mq/mq.sh
@@ -1,3 +1,3 @@
-echo "UserParameter=zkstatus,sudo /usr/bin/docker exec -it zookeeper bash /opt/zookeeper/bin/zkServer.sh status|grep -E 'follower|leader'|wc -l" >> /etc/zabbix/zabbix_agentd.conf 
-echo 'zabbix ALL=(root)NOPASSWD:/usr/bin/docker,/usr/bin/docker exec -it zookeeper bash /opt/zookeeper/bin/zkServer.sh status' >>/etc/sudoers 
+echo "zabbix ALL=(root)NOPASSWD:/usr/bin/docker,/usr/bin/docker exec -it zookeeper bash /opt/zookeeper/bin/zkServer.sh status"  >>  /etc/sudoers
+echo "UserParameter=zkstatus,sudo /usr/bin/docker exec -i zookeeper /opt/zookeeper/bin/zkServer.sh status 2>&1 |awk 'END{print \$2}'" >> /etc/zabbix/zabbix_agentd.conf
 systemctl restart zabbix-agent.service 
