73ba87cee76dedd4c67c867bc2b05b89d435f9d4
Mark <usertzc@gmail.com>
2018-08-31 09:56:19 +0800

    Update downiptables.sh

--- zabbix-install/downiptables.sh
+++ zabbix-install/downiptables.sh
@@ -2,6 +2,7 @@
 # centos7.2
 systemctl mask firewalld
 systemctl stop firewalld
+yum install epel* -y
 yum -y install iptables-devel iptables-services iptables
 systemctl enable iptables
 systemctl start iptables
