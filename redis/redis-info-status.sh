e47a8c42b84aebd0bbc1c498b34245caf2585686
Mark <usertzc@gmail.com>
2018-10-01 15:07:30 +0800

    Update redis-info-status.sh

--- redis/redis-info-status.sh
+++ redis/redis-info-status.sh
@@ -19,8 +19,8 @@ curl -Lk https://raw.githubusercontent.com/LinuxEA-Mark/zabbix3.0.2-complete-wor
 chmod +x ${ZAPATHA}/redis*
 
 if [ `grep redis ${ZAPATH}|wc -l` = 0 ];then
-	echo "UserParameter=redis_info[*],/usr/local/zabbix/scripts/redis_info.sh \$1 \$2" >> ${ZAPATH}
-	echo "UserParameter=redis_hits,/usr/local/zabbix/scripts/redis_hits.sh" >> ${ZAPATH}
+	echo "UserParameter=redis_info[*],${ZAPATHA}/redis_info.sh \$1 \$2" >> ${ZAPATH}
+	echo "UserParameter=redis_hits,${ZAPATHA}/redis_hits.sh" >> ${ZAPATH}
 fi
 
 (crontab -l; echo -e "*/1 * * * * /usr/local/bin/redis-cli -h 127.0.0.1 -p 6379 info > /tmp/redis-info.txt 2>/dev/null" ) | crontab -
