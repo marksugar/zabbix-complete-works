8cc206b7b17c63dd97e5c43d6d347f09f1557b05
Mark <usertzc@gmail.com>
2018-10-01 14:49:06 +0800

    Update redis-info-status.sh

--- redis/redis-info-status.sh
+++ redis/redis-info-status.sh
@@ -6,7 +6,7 @@ ZAPATHA=/etc/zabbix/scripts
 [ -d ${ZAPATHA} ]|| mkdir -p ${ZAPATHA}
 
 if [ `type redis-cli|wc -l` = 0 ];then
-	git clone http://github.com/antirez/redis.git "${REDISPATH}" \
+	git clone http://github.com/antirez/redis.git "${REDISPATH}"
 	cd ${REDISPATH} && git checkout 3.0 && make redis-cli  && cp src/redis-cli /usr/local/bin \rm -rf ${REDISPATH}
 else
     	echo "redis-cli already exists!"
