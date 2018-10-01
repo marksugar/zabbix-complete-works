1ca3289bb53004761109a75a78a59d84ae17ec51
Mark <usertzc@gmail.com>
2018-10-01 11:03:54 +0800

    Create nginx-php-status.sh

---
+++ nginx/nginx-php-status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+NGPATH=/etc/zabbix/scripts
+NGFILE=/etc/zabbix/scripts/nginx_status.sh
+NGSTATUS=/usr/local/nginx/conf/vhost/status.conf
+[ -d ${NGPATH} ]||mkdir ${NGPATH} -p
+[ -f ${NGFILE} ]||curl -Lks https://raw.githubusercontent.com/LinuxEA-Mark/zabbix3.0.2-complete-works/master/nginx/nginx_status.sh -o ${NGFILE} && chmod +x ${NGFILE}
+[ -f ${NGSTATUS} ] || curl  -Lks https://raw.githubusercontent.com/LinuxEA-Mark/zabbix3.0.2-complete-works/master/nginx/status.conf -o ${NGSTATUS}
+
+ZAPATH=/etc/zabbix/zabbix_agentd.conf
+if [ `grep nginx ${ZAPATH}|wc -l` = 0 ];then
+  echo "UserParameter=nginx.status[*],/usr/local/zabbix/scripts/nginx_status.sh \$1 \$2" >> ${ZAPATH}
+fi
+  
+if [ `grep php-fpm ${ZAPATH}|wc -l` = 0 ];then
+  echo "UserParameter=php-fpm.status[*],/usr/bin/curl -s "http://127.0.0.1:40080/php-fpm_status?xml" | grep "<\$1>" | awk -F'>|<' '{ print \$\$3}'" >> ${ZAPATH}
+fi
