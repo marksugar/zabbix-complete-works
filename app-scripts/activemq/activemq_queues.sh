1d1eff343c29259e47d5ce14162124547063d3f0
mark <usertzc@gmail.com>
2020-04-13 16:49:20 +0800

    Adjustment

--- app-scripts/activemq/activemq_queues.sh
+++ app-scripts/activemq/activemq_queues.sh
@@ -7,3 +7,4 @@ mkdir -p /etc/zabbix/scripts
 sed -i 's@#Include=/etc/zabbix/zabbix_agentd.d/*.conf@Include=/etc/zabbix/zabbix_agentd.d/*.conf@g' /etc/zabbix/zabbix_agentd.conf
 curl -LKs https://raw.githubusercontent.com/marksugar/zabbix-complete-works/master/app-scripts/activemq/activemq.conf -o /etc/zabbix/zabbix_agentd.d/activemq.conf
 curl -LKs https://raw.githubusercontent.com/marksugar/zabbix-complete-works/master/app-scripts/activemq/Queues_Name.py -o /etc/zabbix/scripts/Queues_Name.py
+chmod +x /etc/zabbix/scripts/Queues_Name.py
